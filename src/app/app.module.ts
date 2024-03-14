59e5c9870b046b32debb7b30cbb25b3f77de49ab
Sara Ramirez <sara_cabreraramirez@sluz.ch>
2024-03-14 14:49:34 +0100

    Add frontend

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import {NavigationComponent} from "./navigation/navigation.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    NavigationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
