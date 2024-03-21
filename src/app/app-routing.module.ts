4e5136eaee93cdf2a55a83af0eaca41287e2554e
Mathusiga Nithivathanan <mathusiga_nithivath@sluz.ch>
2024-03-21 16:16:53 +0100

    log/sign btn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LayoutComponent} from "./layout/layout.component";
 import { FooterComponent} from "./footer/footer.component";
 import { NavigationComponent} from "./navigation/navigation.component";
 import { SearchBarComponent} from "./search-bar/search-bar.component";
+import {HeaderComponent} from "./header/header.component";
 
 
 const routes: Routes = [
   {path: 'layout', component: LayoutComponent},
   {path: 'footer', component: FooterComponent},
   {path: 'navigation', component: NavigationComponent},
-  {path: 'search-bar', component:SearchBarComponent}
+  {path: 'search-bar', component:SearchBarComponent},
+  {path: 'header', component:HeaderComponent}
 ];
 
 @NgModule({
