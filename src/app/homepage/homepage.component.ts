2518f7b03913a44381ecbd27e5c10f105353ba5c
Aline Schwegler <aline_schwegler@sluz.ch>
2024-03-28 15:46:40 +0100

    frontend for popup is done

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {UploadPopupComponent} from "../upload-popup/upload-popup.component";
+
 
 
 @Component({
@@ -9,13 +12,16 @@ import {Router} from "@angular/router";
 })
 export class HomepageComponent {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private modalService: NgbModal) {}
 
+  openCreatePostModal() {
+    this.modalService.open(UploadPopupComponent);
+  }
   navigateToAbout(): void {
     this.router.navigateByUrl('/about');
   }
-
   navigateToExplore(): void {
     this.router.navigateByUrl('/explore');
   }
+
 }
