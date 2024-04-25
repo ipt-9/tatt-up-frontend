f34396f3a8742cd92cb2b9f41eb6ab55d84dc456
Sara Ramirez <sara_cabreraramirez@sluz.ch>
2024-04-25 14:03:17 +0200

    Changed things

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 /// <reference types="@angular/localize" />
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
+
+
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
